49d53aa199c081f6f4cb0b24922455904efb8d0c
Irfan Quresh <irfan.quresh@datamigrationresources.com>
2020-07-05 01:25:52 +0530

    Springboot service app Initialization...

---
+++ src/main/java/com/intellocent/springboot/SpringbootCoreLearningseriesApplication.java
@@ -0,0 +1,13 @@
+package com.intellocent.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCoreLearningseriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCoreLearningseriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intellocent/springboot/SpringbootCoreLearningseriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intellocent.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCoreLearningseriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
